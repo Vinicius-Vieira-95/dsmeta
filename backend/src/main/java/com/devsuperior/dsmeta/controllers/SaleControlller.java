4620c7bfde36ebb946a6dd35ac06a553db5b68a1
Vinicius Vieira Carneiro <vinicius.95vieira00@gmail.com>
2022-07-14 09:42:55 -0300

    Environment setting

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleControlller.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleControlller.java
@@ -1,12 +1,9 @@
 package com.devsuperior.dsmeta.controllers;
 
-import javax.sound.midi.Patch;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
